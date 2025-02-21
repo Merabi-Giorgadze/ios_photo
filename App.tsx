d435e7eb9c1cd8f610f24b969cf34c3cdc6ba580
Merabi-Giorgadze <124910119+Merabi-Giorgadze@users.noreply.github.com>
2025-02-21 19:29:28 +0400

    main

---
+++ App.tsx
@@ -0,0 +1,189 @@
+import {
+  CameraMode,
+  CameraType,
+  CameraView,
+  useCameraPermissions,
+} from "expo-camera";
+import { useRef, useState } from "react";
+import { Button, Pressable, StyleSheet, Text, View } from "react-native";
+import { Image } from "expo-image";
+import { AntDesign } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import { FontAwesome6 } from "@expo/vector-icons";
+
+export default function App() {
+  const [permission, requestPermission] = useCameraPermissions();
+  const ref = useRef<CameraView>(null);
+  const [uri, setUri] = useState<string | null>(null);
+  const [mode, setMode] = useState<CameraMode>("picture");
+  const [facing, setFacing] = useState<CameraType>("back");
+  const [recording, setRecording] = useState(false);
+
+  if (!permission) {
+    return null;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.permissionText}>
+          We need your permission to use the camera
+        </Text>
+        <Button onPress={requestPermission} title="Grant permission" />
+      </View>
+    );
+  }
+
+  const takePicture = async () => {
+    const photo = await ref.current?.takePictureAsync();
+    setUri(photo?.uri);
+  };
+
+  const recordVideo = async () => {
+    if (recording) {
+      setRecording(false);
+      ref.current?.stopRecording();
+      return;
+    }
+    setRecording(true);
+    const video = await ref.current?.recordAsync();
+    console.log({ video });
+  };
+
+  const toggleMode = () => {
+    setMode((prev) => (prev === "picture" ? "video" : "picture"));
+  };
+
+  const toggleFacing = () => {
+    setFacing((prev) => (prev === "back" ? "front" : "back"));
+  };
+
+  const renderPicture = () => {
+    return (
+      <View style={styles.pictureContainer}>
+        <Image
+          source={{ uri }}
+          contentFit="contain"
+          style={styles.imagePreview}
+        />
+        <Button onPress={() => setUri(null)} title="Take another picture" />
+      </View>
+    );
+  };
+
+  const renderCamera = () => {
+    return (
+      <CameraView
+        style={styles.camera}
+        ref={ref}
+        mode={mode}
+        facing={facing}
+        mute={false}
+        responsiveOrientationWhenOrientationLocked
+      >
+        <View style={styles.shutterContainer}>
+          <Pressable onPress={toggleMode} style={styles.modeButton}>
+            {mode === "picture" ? (
+              <AntDesign name="picture" size={32} color="white" />
+            ) : (
+              <Feather name="video" size={32} color="white" />
+            )}
+          </Pressable>
+
+          <Pressable
+            onPress={mode === "picture" ? takePicture : recordVideo}
+            style={({ pressed }) => [
+              styles.shutterBtn,
+              {
+                opacity: pressed ? 0.7 : 1,
+              },
+            ]}
+          >
+            <View
+              style={[
+                styles.shutterBtnInner,
+                {
+                  backgroundColor: mode === "picture" ? "white" : "red",
+                },
+              ]}
+            />
+          </Pressable>
+
+          <Pressable onPress={toggleFacing} style={styles.switchButton}>
+            <FontAwesome6 name="rotate-left" size={32} color="white" />
+          </Pressable>
+        </View>
+      </CameraView>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      {uri ? renderPicture() : renderCamera()}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  permissionText: {
+    color: "white",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  camera: {
+    flex: 1,
+    width: "100%",
+  },
+  shutterContainer: {
+    position: "absolute",
+    bottom: 40,
+    left: 0,
+    right: 0,
+    width: "100%",
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 30,
+  },
+  modeButton: {
+    padding: 10,
+    backgroundColor: "rgba(0, 0, 0, 0.4)",
+    borderRadius: 50,
+  },
+  shutterBtn: {
+    backgroundColor: "transparent",
+    borderWidth: 5,
+    borderColor: "white",
+    width: 85,
+    height: 85,
+    borderRadius: 45,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  shutterBtnInner: {
+    width: 70,
+    height: 70,
+    borderRadius: 50,
+  },
+  switchButton: {
+    padding: 10,
+    backgroundColor: "rgba(0, 0, 0, 0.4)",
+    borderRadius: 50,
+  },
+  pictureContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imagePreview: {
+    width: 300,
+    height: 300,
+    marginBottom: 20,
+  },
+});
